b5f261e84aff439d85f41418b58933c43e12e6b7
lishuangqi <349676554@qq.com>
2017-10-16 10:57:06 +0800

    delete

--- myutils/src/main/java/com/lishuangqi/utils/Base64Util.java
+++ myutils/src/main/java/com/lishuangqi/utils/Base64Util.java
@@ -37,18 +37,7 @@ public class Base64Util {
   
     // 解密  
     public static String getFromBase64(String s) {  
-        /*byte[] b = null;  
-        String result = null;  
-        if (s != null) {  
-            BASE64Decoder decoder = new BASE64Decoder();  
-            try {  
-                b = decoder.decodeBuffer(s);  
-                result = new String(b, "utf-8");  
-            } catch (Exception e) {  
-                e.printStackTrace();  
-            }  
-        }*/  
-    	String result = null; 
+    	String result = null;
     	try {
 			result = new String(Base64.decodeBase64(s.getBytes("UTF-8")));
 		} catch (UnsupportedEncodingException e) {
