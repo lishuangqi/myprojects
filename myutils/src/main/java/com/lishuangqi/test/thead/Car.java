a39762757fcaf19c8d3d0bd752910971d09f27ef
lishuangqi <349676554@qq.com>
2019-04-18 17:46:51 +0800

    add completableFutureExamples

---
+++ myutils/src/main/java/com/lishuangqi/test/thead/Car.java
@@ -0,0 +1,29 @@
+package com.lishuangqi.test.thead;
+
+/**
+ * Created by michael on 2019/4/18.
+ */
+public class Car {
+    int id;
+    int manufacturerId;
+    String model;
+    int year;
+    float rating;
+
+    public Car(int id, int manufacturerId, String model, int year) {
+        this.id = id;
+        this.manufacturerId = manufacturerId;
+        this.model = model;
+        this.year = year;
+    }
+
+    void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Car (id=" + id + ", manufacturerId=" + manufacturerId + ", model=" + model + ", year=" + year
+                + ", rating=" + rating;
+    }
+}
